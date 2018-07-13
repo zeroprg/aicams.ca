e0023476d7317f10b45b8fcf7f6488ab2d0c58a8
Vitaly Arkhipov <ark0006@cpr.ca>
2018-07-13 11:26:45 -0600

    new Delete bttn added

--- js/app.js
+++ js/app.js
@@ -3,31 +3,42 @@ let API = "http://aicams.ca:5000/"
 let DEFAULT_QUERY ="urls?list=1"
 
 var URLlist = React.createClass({
-  getInitialState:function(){
-    return{
-      sortedArr:[],
-      regArray:[],
-      data: []
-    }
-  },
   
-  componentDidMount() {
-    this.setState({ isLoading: true });
-    console.log(" start:" )
-    fetch(API + DEFAULT_QUERY)
-      .then(response => {
-        console.log(" response:" + response )
-        if (response.ok) {
-          //console.log(" response:" + JSON.stringify(response, null, 2) )
-          return response.json();
-        } else {
-          console.log(" error:" )
-          throw new Error('Something went wrong ...');
+    getInitialState:function(){
+        return{
+          sortedArr:[],
+          regArray:[],
+          data: [],
+          delBttn: '<span class= "glyphicon glyphicon-remove" aria-hidden="true"></span><span><strong>Delete</strong></span>',      
         }
-      })
-      .then(data => this.setState({ data: data, isLoading: false }))
-      .catch(error => this.setState({ error, isLoading: false }));
-  },
+    },
+
+    buttonClick() {
+        console.log("Bttn clicked here");
+        loadData();
+    },
+
+    loadData(){
+        this.setState({ isLoading: true });
+        console.log(" start:")
+        fetch(API + DEFAULT_QUERY)
+            .then(response => {
+                console.log(" response:" + response)
+                if (response.ok) {
+                    //console.log(" response:" + JSON.stringify(response, null, 2) )
+                    return response.json();
+                } else {
+                    console.log(" error:")
+                    throw new Error('Something went wrong ...');
+                }
+            })
+            .then(data => this.setState({ data: data, isLoading: false }))
+            .catch(error => this.setState({ error, isLoading: false }));
+    },
+
+    componentDidMount() {
+        loadData();
+     },
   
      render() {
       const { data, isLoading, error} = this.state;
@@ -38,17 +49,13 @@ var URLlist = React.createClass({
 
       if (isLoading) {
         return <p>Loading ...</p>;
-      }
-
+         }
       return (
         <ul>
           {data.map(data =>
             <li key={data}>
-              <a href={data[1]}>{data[1]}</a>
-                  <a href="#" class="btn btn-primary a-btn-slide-text">
-                      <span class="glyphicon glyphicon-edit" aria-hidden="true"></span>
-                      <span><strong>Edit</strong></span>            
-                  </a>        
+            <a href={data[1]}>{data[1]}</a>
+            &nbsp;<a href="//aicams.ca:5000/urls?delete={data[1]}" onClick={this.buttonClick.bind(this)} class="btn btn-primary a-btn - slide - text" dangerouslySetInnerHTML={{ __html: this.state.delBttn }} />
             </li>
           )}
         </ul>
