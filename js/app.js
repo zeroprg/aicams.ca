1907406ba7f8060428fc58db20f4107d8aff5438
Vitaly <zeroprg@yahoo.com>
2023-01-24 11:17:51 -0700

    f

--- js/app.js
+++ js/app.js
@@ -67,7 +67,7 @@ var URLlist = React.createClass({
         return (
             <ul>
                 {data.map(data =>
-                    <li key={data['url']}>
+                    <li key={data['cam']}>key={data['cam']}
                         <a href={data['url']}>{data['url']}</a>
                         &nbsp;
                             <a href="#" onClick={() => this.buttonClick(this.state.url+data['url'])} className="btn btn-primary a-btn - slide - text">
